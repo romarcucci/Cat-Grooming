ed483e39eab4f41df607666edfdee46030332869
Romain Marcucci <romain.marcucci@gmail.com>
2017-11-09 20:51:26 +0100

    activités

--- src/App.js
+++ src/App.js
@@ -13,12 +13,68 @@ class Header extends Component {
   )};
 }
 
+class Activity extends Component {
+  render() {
+    const names = [
+      'Couper griffes', 
+      'Toilettage poils long', 
+      'Toilettage poils court',
+      'Vernissage',
+      'Massage'];
+    const prices = [15, 70, 50, 25, 30];
+    const images = [
+      './img/griffes.png',
+      './img/long.png',
+      './img/court.png',
+      './img/vernis.png',
+      './img/massage.png'
+    ];
+    return(
+      <div className="activity">
+        <span className="activity-name">
+          {names[this.props.id]} 
+        </span>
+        <span className="activity-price">
+          {prices[this.props.id]} €
+        </span>
+        <img src={require(''+images[this.props.id])} className="activity-image" alt={names[this.props.id]} />
+      </div>
+  )};
+}
+
+class Activities extends Component {
+
+  render(){
+    const numbers = [0,1,2,3,4];
+    return(
+      <div className="activities">
+        <h2>Nos Activités</h2>
+        <table align="center">
+          <tr>
+            {numbers.map(function(number, index){
+              return <td><Activity id={ index } /></td>;
+            })}
+          </tr>
+        </table>
+      </div>
+    )
+  }
+}
+
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        bttnText: "Prendre rendez-vous"
+    };
+}
   render() {
     return (
       <div className="App">
         <Header />
+        <Activities />
+        <button> {this.state.bttnText}</button>
       </div>
     );
   }
