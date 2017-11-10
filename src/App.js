c021e39badf41ccdf735cf7e7751aa4c4040d919
Romain Marcucci <romain.marcucci@gmail.com>
2017-11-10 11:47:13 +0100

    message de fin

--- src/App.js
+++ src/App.js
@@ -63,42 +63,90 @@ class Activities extends Component {
   }
 }
 
-class Form extends Component {
-  
+class Message extends Component {
+
   render(){
     return(
-      <div className="activities">
+      <div>
         <h2>Prendre rendez-vous</h2>
-        <form align="center">
-          Prenom: <br/>
-          <input type="text" name="firstname"/>
-          <br/>
-          Nom:<br/>
-          <input type="text" name="lastname"/>
-          <br/>
-          Date:<br/>
-          <input type="date" name="date"/>
-          <br/>
-          Telephone:<br/>
-          <input id="phonenum" type="tel" pattern="^\d{10}$" required />
-          <br/>
-          e-mail:<br/>
-          <input type="email" name="email" required placeholder="Enter a valid email address"/>
-          <br/>
-          Traitements:
-          <br/>
-          Couper les griffes<input type="checkbox" value="griffes"/>
-          Toilettage<input type="checkbox" value="toilettage"/>
-          Vernis<input type="checkbox" value="vernis"/>
-          Massage<input type="checkbox" value="massage"/>
-          <br/>
-          Taille poils:
-          <br/>
-          <input type="radio" name="poil" value="long"/> Long
-          <input type="radio" name="poil" value="short"/> Court    
-          <br/><br/>
-          <input type="submit" value="Valider rendez-vous"/>
-        </form> 
+        <p>Merci {this.props.prenom}, votre rdv le {this.props.date} est confirmé</p>
+      </div>
+    )
+  };
+}
+
+class Form extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showForm: true,
+      showMessage: false,
+      prenom: '',
+      nom: '',
+      date: null,
+
+
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.setState({showForm: false, showMessage:true});
+  }
+
+
+  render(){
+    return(
+      <div>
+        { this.state.showMessage ? <Message prenom={this.state.prenom} date={this.state.date}/> : null }
+        { this.state.showForm ?  
+          <div>
+            <h2>Prendre rendez-vous</h2>
+            <form align="center" onSubmit={this.handleSubmit}>
+              Prenom: <br/>
+              <input type="text" name="prenom" value={this.state.prenom} onChange={this.handleChange}/>
+              <br/> 
+              Nom:<br/>
+              <input type="text" name="nom" value={this.state.nom} onChange={this.handleChange}/>
+              <br/>
+              Date:<br/>
+              <input type="date" name="date" value={this.state.date} onChange={this.handleChange}/>
+              <br/>
+              Telephone:<br/>
+              <input id="phonenum" type="tel" pattern="^\d{10}$" required />
+              <br/>
+              e-mail:<br/>
+              <input type="email" name="email" required placeholder="Enter a valid email address"/>
+              <br/>
+              Traitements:
+              <br/>
+              Couper les griffes<input type="checkbox" value="griffes"/>
+              Toilettage<input type="checkbox" value="toilettage"/>
+              Vernis<input type="checkbox" value="vernis"/>
+              Massage<input type="checkbox" value="massage"/>
+              <br/>
+              Taille poils:
+              <br/>
+              <input type="radio" name="poil" value="long"/> Long
+              <input type="radio" name="poil" value="short"/> Court    
+              <br/><br/>
+              <input type="submit" value="Valider rendez-vous"/>
+            </form>
+          </div> 
+        : null }
       </div>
     )
   }
@@ -109,32 +157,16 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      step: 1,
-      bttnText: "Prendre rendez-vous",
       showActivities: true,
       showForm: false
     };
   }
 
   click() {
-    switch(this.state.step){
-      case 1:
-        this.setState({
-          bttnText: "Valider rendez-vous",
-          step: 2,
-          showActivities: false,
-          showForm: true
-        });
-        break;
-      case 2:
-        this.setState({
-          bttnText: "",
-          step: 3
-        });
-        break;
-      default:
-        break;
-    }
+    this.setState({
+      showActivities: false,
+      showForm: true
+    });
   }
 
   render() {
@@ -142,7 +174,7 @@ class App extends Component {
       <div className="App">
         <Header />        
         { this.state.showActivities ? <Activities /> : null }
-        { this.state.showActivities ? <button onClick={() => this.click()}> {this.state.bttnText}</button> : null }      
+        { this.state.showActivities ? <button onClick={() => this.click()}> Prendre rendez-vous</button> : null }      
         { this.state.showForm ? <Form /> : null }
       </div>
     );
